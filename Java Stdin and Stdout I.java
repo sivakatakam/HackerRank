bf9f70a6c458fc4f05f6caadadd39ddb18e66866
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-03-07 06:05:10 +0530

    Add files via upload

---
+++ Java Stdin and Stdout I.java
@@ -0,0 +1,15 @@
+import java.io.*;
+class Solution
+{
+    public static void main(String args[])throws Exception
+    {
+        int a,b,c;
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        a=Integer.parseInt(br.readLine());
+        b=Integer.parseInt(br.readLine());
+        c=Integer.parseInt(br.readLine());
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
